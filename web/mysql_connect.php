7aacb5b8412830a758e2d4aa92af97af0713c811
fjkk <fujikake.takayoshi@gmail.com>
2021-04-26 20:53:21 +0900

    mysql connect test4

--- web/mysql_connect.php
+++ web/mysql_connect.php
@@ -6,14 +6,13 @@
 // 93abc74
 // 'bb1251ea55e1d8'@'ip-10-0-118-207.ec2.internal' 
 
-// $dsn = 'mysql:host=us-cdbr-east-03.cleardb.com;dbname=heroku_053601649a23275;charset=utf8';
-// $db_user = 'bb1251ea55e1d8';
-// $db_pass = '93abc748';
-// $dbh = new PDO($dsn, $db_user, $db_pass);
+$dsn = 'mysql:host=us-cdbr-east-03.cleardb.com;dbname=heroku_053601649a23275;charset=utf8';
+$db_user = 'bb1251ea55e1d8';
+$db_pass = '93abc748';
+$dbh = new PDO($dsn, $db_user, $db_pass);
 
 
-
-$dbh = mysqli_connect('us-cdbr-east-03.cleardb.com', 'bb1251ea55e1d8', '93abc748', 'heroku_053601649a23275');
+// $dbh = mysqli_connect('us-cdbr-east-03.cleardb.com', 'bb1251ea55e1d8', '93abc748', 'heroku_053601649a23275');
 
 
 var_dump($dbh);
