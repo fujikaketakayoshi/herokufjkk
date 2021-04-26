07ecf63c50be3d5a5641a7d5e792f636d9146bf5
fjkk <fujikake.takayoshi@gmail.com>
2021-04-26 20:38:37 +0900

    testing

--- web/index.php
+++ web/index.php
@@ -1,5 +1,11 @@
 <?php
 
+//CLEARDB_DATABASE_URL: mysql://bb1251ea55e1d8:93abc748@us-cdbr-east-03.cleardb.com/heroku_053601649a23275?reconnect=true
 
+// mysql -u bb1251ea55e1d8 -p -h us-cdbr-east-03.cleardb.com heroku_053601649a23275
+// 93abc74
+// 'bb1251ea55e1d8'@'ip-10-0-118-207.ec2.internal' 
+
+echo 'hello';
 
 phpinfo();
